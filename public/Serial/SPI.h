5c2a4b4b4dbf1dc47aca477eb1c804322030d8d8
David A. Haufe <david.haufe90@gmail.com>
2024-11-13 18:35:16 +0100

    serial::spi added

---
+++ public/Serial/SPI.h
@@ -0,0 +1,66 @@
+#ifndef PUBLIC_SERIAL_SPI_H_
+#define PUBLIC_SERIAL_SPI_H_
+
+#include <avr/io.h>
+
+namespace serial
+{
+  enum class SPI_mode : uint8_t
+  {
+    kMaster = (1 << MSTR),
+    kSlave = (0 << MSTR)
+  };
+
+  enum class SPI_data_order : uint8_t
+  {
+    kMsb_first = (0 << DORD),
+    kLsb_first = (1 << DORD)
+  };
+
+  enum class SPI_clock_polarity : uint8_t
+  {
+    kIdle_low = (0 << CPOL),
+    kIdle_high = (1 << CPOL)
+  };
+
+  enum class SPI_clock_phase : uint8_t
+  {
+    kLeading = (0 << CPHA),
+    kTrailing = (1 << CPHA)
+  };
+
+  enum class SPI_clock_rate : uint8_t
+  {
+    k8mHz = (1 << SPI2X) | (0 << SPR1) | (0 << SPR0),
+    k4mHz = (0 << SPR1) | (0 << SPR0),
+    k2mHz = (1 << SPI2X) | (0 << SPR1) | (1 << SPR0),
+    k1mHz = (0 << SPR1) | (1 << SPR0),
+    k500kHz = (1 << SPI2X) | (1 << SPR1) | (1 << SPR0),
+    k250kHz = (1 << SPR1) | (0 << SPR0),
+    k125kHz = (1 << SPR1) | (1 << SPR0)
+  };
+
+  struct SPI_parameters
+  {
+    SPI_mode spi_mode = SPI_mode::kMaster;
+    SPI_data_order data_order = SPI_data_order::kMsb_first;
+    SPI_clock_polarity clock_polarity = SPI_clock_polarity::kIdle_low;
+    SPI_clock_phase clock_phase = SPI_clock_phase::kLeading;
+    SPI_clock_rate clock_rate = SPI_clock_rate::k4mHz;
+  };
+
+  class SPI
+  {
+   public:
+    SPI(const SPI_parameters &parameters);
+    ~SPI() = default;
+   public:
+    const constexpr uint8_t kMOSI = (1 << DDB3);
+    const constexpr uint8_t kMISO = (0 << DDB4);
+    const constexpr uint8_t kSCK = (1 << DDB5);
+    const constexpr uint8_t kSS = (1 << DDB2);
+  };
+
+} // namespace serial
+
+#endif // PUBLIC_SERIAL_SPI_H_
---
+++ src/Serial/SPI.cpp
@@ -0,0 +1,25 @@
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/wdt.h>
+#include <stdint.h>
+
+#include "Serial/SPI.h"
+
+namespace serial {
+
+SPI::SPI(const SPI_parameters &parameters) {
+
+  // Set MOSI and SCK output, all others input
+  DDR_SPI = (1 << DDB3) | (1 << DDB5) | (1 << DDB2);
+  DDR_SPI = kMOSI | kSCK | kSS | kMISO;
+
+  // Enable SPI, Master, set clock rate fck/16
+  SPCR = static_cast<uint8_t>(parameters.spi_mode) |
+         static_cast<uint8_t>(parameters.data_order) |
+         static_cast<uint8_t>(parameters.clock_polarity) |
+         static_cast<uint8_t>(parameters.clock_phase) |
+         static_cast<uint8_t>(parameters.clock_rate) |
+         (1 << SPE)
+
+
+} // namespace serial
